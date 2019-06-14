925361482e310d5b5a67b3ece12b757f767ae08a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-06-14 22:16:40 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -8,7 +8,6 @@ var currentGame = 0;
 var WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 
-
 function player() {
   if (turn % 2 == 0) {
     return "X";
